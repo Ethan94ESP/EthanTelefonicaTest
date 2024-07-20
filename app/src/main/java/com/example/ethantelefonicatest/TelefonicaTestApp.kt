b3b60091481b20a40e9ed0bfe2ba8e76b790157a
Ethan94ESP <ethancrespopueyo@gmail.com>
2024-07-20 11:37:49 +0200

    Initial commit setting up the dependencies with a clean and scalable project structure

---
+++ app/src/main/java/com/example/ethantelefonicatest/TelefonicaTestApp.kt
@@ -0,0 +1,7 @@
+package com.example.ethantelefonicatest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TelefonicaTestApp: Application()
---
+++ app/src/main/java/com/example/ethantelefonicatest/data/network/ReqresApi.kt
@@ -0,0 +1,7 @@
+package com.example.ethantelefonicatest.data.network
+
+interface ReqresApi {
+    companion object{
+        const val baseUrl = "https://reqres.in/"
+    }
+}
