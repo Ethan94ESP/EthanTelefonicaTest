198939582d63c567219524a4afbe7f4dd2bf962e
Ethan94ESP <ethancrespopueyo@gmail.com>
2024-07-20 14:00:26 +0200

    Second Screen working and showing results

--- app/src/main/java/com/example/ethantelefonicatest/domain/model/UserBO.kt
+++ app/src/main/java/com/example/ethantelefonicatest/domain/model/UserBO.kt
@@ -2,7 +2,7 @@ package com.example.ethantelefonicatest.domain.model
 
 
 data class UserBO(
-    val id: Int,
+    val id: String,
     val name: String,
     val email: String,
     val image: String
